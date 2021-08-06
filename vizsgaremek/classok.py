fac3136371f5b039a9b85f1c3ff6eafe8596a4f4
Kincses László <kincsesster@gmail.com>
2021-08-06 21:12:10 +0200

    Regisztráció utáni belépés.

--- vizsgaremek/classok.py
+++ vizsgaremek/classok.py
@@ -49,15 +49,22 @@ class TestSign_upLap(object):  # A regisztráció teszteléséhez.
             self.hibaablak()
             log = self.reszlet.text == d
             self.failed_okgomb.click()
-            return log
         else:
             time.sleep(3)
             self.sikerablak()
             log = self.successful.text == lokatorok.sikerszoveg
             self.successful_okgomb.click()
-            self.driver.refresh()  # Újratöltöm az ablakot (nem tudom, miért, de így megy.
-            self.driver.find_element_by_xpath(lokatorok.logout).click()  # Logoutolok
-            return log
+            self.driver.refresh()  # Újratöltöm az ablakot (nem tudom, miért, de így megy).
+            time.sleep(2)
+            self.egyiklogout = self.driver.find_elements_by_xpath(lokatorok.logout) # Logoutolok
+            self.masiklogout = self.driver.find_elements_by_xpath(lokatorok.logout0) # Logoutolok
+            if len(self.egyiklogout)>0:
+                self.egyiklogout[0].click()
+            elif len(self.egyiklogout)>0:
+                self.masiklogout[0].click()
+            else:
+                log = False
+        return log
 
 
 class TestSign_inLap(object):  # A bejelentkezés teszteléséhez.
@@ -72,3 +79,11 @@ class TestSign_inLap(object):  # A bejelentkezés teszteléséhez.
 
     def teardown(self):  # Lerombolás.
         self.driver.close()
+
+    def bejelentkezik(self, a, b):
+        self.emil.send_keys(a)
+        self.password.send_keys(b)
+        self.submit.click()
+        time.sleep(3)
+        self.driver.refresh()
+        return len(self.driver.find_elements_by_xpath(lokatorok.logout)) > 0
--- vizsgaremek/test_signup_jok.py
+++ vizsgaremek/test_signup_jok.py
@@ -2,12 +2,32 @@ import time
 import randomstring
 import classok
 
-
+"""
 def test_reg_mezok01():
     lap = classok.TestSign_upLap()
     lap.setup()
-    time.sleep(2)
+    time.sleep(3)
     assert lap.test_01_reg_mezok(randomstring.nev(), randomstring.emil(), randomstring.passw(), "")
-    time.sleep(2)
+    time.sleep(3)
+    lap.teardown()
+"""
+
+a = randomstring.nev()
+b = randomstring.emil()
+c = randomstring.passw()
+
+
+def test_reg_mezok01():  # Érvényes regisztráció + bejelentkezés.
+    lap = classok.TestSign_upLap()
+    lap.setup()
+    time.sleep(3)
+    log = lap.test_01_reg_mezok(a, b, c, "")
+    time.sleep(3)
+    lap.teardown()  # A regisztráció bezár.
+    lap = classok.TestSign_inLap()  # A bejelentkezés nyit.
+    lap.setup()
+    time.sleep(3)
+    log = log and lap.bejelentkezik(b, c)
+    assert log
     lap.teardown()
 
