f72c57cec62d01fa4b55b086c70c00e67e617407
Kincses László <kincsesster@gmail.com>
2021-08-12 17:47:58 +0200

    A véletlen adatok UTF-ben vannak.

--- vizsgaremek/veletlen_adatok_csvbe.py
+++ vizsgaremek/veletlen_adatok_csvbe.py
@@ -2,7 +2,7 @@ import randomstring
 
 with open("veletlen_adatok.csv", "w") as file:  # 10 véletlen regisztrációs adatot hoz létre és ment csv-be.
     for i in range(10):
-        a = randomstring.nev()
+        a = randomstring.name()
         b = randomstring.emil()
         c = randomstring.passw()
         s = a + ";" + b + ";" + c + "\n"
